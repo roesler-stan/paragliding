8852777aef9e0d50f9089800ccd24a0c6043ae6e
Katharina Roesler <roesler@stanford.edu>
2018-03-04 15:05:10 -0800

    First commit for fork to convert IGC files to Google Earth KML files.

--- track.py
+++ track.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 # ex:set fileencoding=utf-8:
 
+"""
+python track.py files ed
+"""
+
 from __future__ import unicode_literals
 
 import argparse
 import os
-
-from zipfile import ZipFile
-from zipfile import ZIP_DEFLATED
 
 from paragliding.parsers import FlightLog
 from paragliding.parsers import Flight
@@ -15,7 +16,8 @@ from paragliding.parsers import Flight
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process a directory with igc files')
-    parser.add_argument('dir', metavar='<dir>', type=str, help='directory')
+    parser.add_argument('dir', type=str, help='directory')
+    parser.add_argument('output', type=str, help='output filename')
     args = parser.parse_args()
 
     flights = FlightLog()
@@ -25,7 +27,4 @@ if __name__ == '__main__':
                 f = Flight(os.path.join(path, file), file)
                 flights.add_flight(f)
     flights.make_tree()
-    flights.write('test.kml')
-    with ZipFile('test.kmz', 'w', ZIP_DEFLATED) as myzip:
-        myzip.write('test.kml')
-    os.remove('test.kml')
+    flights.write(args.output + '.kml')
